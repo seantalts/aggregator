5c80a0f3fafa993814c2144271dca14b16123faa
Sean Talts <sean@lot18.com>
2013-12-04 12:49:26 -0500

    if we run out of dates, end gracefully in stat printing

--- aggregator.py
+++ aggregator.py
@@ -5,20 +5,20 @@ from datetime import datetime
 
 
 class Aggregator(object):
-    def __init__(self, targets):
-        self.targets = targets
+    def __init__(self, fields):
+        self.fields = fields
         self.stats = defaultdict(int)
         self.start_date = datetime(9999, 1, 1)
         self.end_date = datetime(1, 1, 1)
 
     def __str__(self):
-        return "Aggregator(%s)" % (", ".join(map(str, self.targets)),)
+        return "Aggregator(%s)" % (", ".join(map(str, self.fields)),)
 
     def add(self, d):
-        for target in self.targets:
-            val = target.value(d)
+        for field in self.fields:
+            val = field.value(d)
             if val:
-                self.stats[target.key(d)] += val  #XXX use generic aggregator functions
+                self.stats[field.key(d)] += val  #XXX use generic aggregator functions
                 self.start_date = min(self.start_date, d['time'])
                 self.end_date = max(self.end_date, d['time'])
 
@@ -33,7 +33,10 @@ class Aggregator(object):
                 for d in date_iterator(self.start_date, self.end_date, duration):
                     if curr_key.time == d:
                         print curr_val,
-                        curr_key, curr_val = stats.next()
+                        try:
+                            curr_key, curr_val = stats.next()
+                        except StopIteration:
+                            continue
                     else:
                         print 0,
                 print
