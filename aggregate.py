c68625e8bde85ca99a22af4048a8a84245bffdcd
Sean Talts <sean@lot18.com>
2013-12-04 12:09:38 -0500

    Print out over date range

--- aggregate.py
+++ aggregate.py
@@ -3,8 +3,9 @@
 import argparse
 from datetime import datetime
 import utils
-from field import Field
+from field import Field, date_iterator
 from collections import defaultdict
+from itertools import groupby
 try:
     import simplejson as json
 except ImportError:
@@ -49,16 +50,42 @@ class Aggregator(object):
     def __init__(self, targets):
         self.targets = targets
         self.stats = defaultdict(int)
+        self.start_date = datetime(9999, 1, 1)
+        self.end_date = datetime(1, 1, 1)
 
     def __str__(self):
         return "Aggregator(%s)" % (", ".join(map(str, self.targets)),)
-
 
     def add(self, d):
         for target in self.targets:
             val = target.value(d)
             if val:
                 self.stats[target.key(d)] += val  #XXX use generic aggregator functions
+                self.start_date = min(self.start_date, d['time'])
+                self.end_date = max(self.end_date, d['time'])
+
+    def print_stats(self):
+        for key, stats in groupby(sorted(self.stats.iteritems()),
+                                  key=lambda (k, v): (k.target, k.owner, k.duration)):
+            target, owner, duration = key
+            print
+            print "%s's %ss by %s" % (
+                owner,
+                target,
+                duration or "all time")
+            curr_key, curr_val = stats.next()
+            if duration:  #if there is a duration
+                for d in date_iterator(self.start_date, self.end_date, duration):
+                    if curr_key.time == d:
+                        print curr_val,
+                        curr_key, curr_val = stats.next()
+                    else:
+                        print 0,
+                print
+            else:
+                print curr_val
+
+
 
 
 if __name__ == "__main__":
@@ -67,5 +94,4 @@ if __name__ == "__main__":
     line_parser = utils.and_then(json.loads, convert_timestamp)
     for r in iterate_records(args.filename, line_parser):
         aggregator.add(r)
-    for s, v in sorted(aggregator.stats.iteritems()):
-        print s, ": ", v
+    aggregator.print_stats()
--- field.py
+++ field.py
@@ -1,5 +1,32 @@
 """Field module for specifying fields to aggregate"""
-import itertools
+from collections import namedtuple
+from datetime import timedelta, datetime
+
+
+Key = namedtuple("Stat", ['target', 'owner', 'duration', 'time'])
+DURATIONS=["year", "month", "day", "hour", "minute"]
+TYPES=("follow", "favorite", "order")
+
+
+def _relevant_durations(duration):
+    #it's important to use all the parts of a date larger than the target duration
+    #in order to identify the duration uniquely
+    return DURATIONS[:DURATIONS.index(duration) + 1]
+
+
+def _construct_relevant_date(dt, duration):
+    return datetime(*(getattr(dt, d) for d in _relevant_durations(duration)))
+
+
+def date_iterator(start, end, duration):
+    start =  _construct_relevant_date(start, duration) # zero out irrelevant parts
+    end = _construct_relevant_date(end, duration)
+    curr = start
+    delta = timedelta(**{duration + "s": 1})
+    while curr < end:
+        yield curr
+        curr += delta
+
 
 def _count_type(t):
     def _dict_count(d):
@@ -11,10 +38,11 @@ def _count_type(t):
 
 
 class Field(object):
-    TYPES=("follow", "favorite", "order")
+
     TARGET_FINDERS={t: _count_type(t) for t in TYPES}
     TARGET_FINDERS["total"] = lambda d: d['total'] if 'total' in d else 0
-    DURATIONS=["year", "month", "day", "hour", "minute"]
+    #Add more finders here when needed
+
 
     def __init__(self, field_string):
         parts = field_string.split("/")
@@ -25,29 +53,29 @@ class Field(object):
             self.owner = parts[1]
         else:
             self.duration = parts[1]
-            assert self.duration in self.DURATIONS, "Duration must be one of %s" % (
-                self.DURATIONS,)
+            assert self.duration in DURATIONS, "Duration must be one of %s" % (
+                DURATIONS,)
             self.owner = parts[2]
 
         self.value = self.TARGET_FINDERS[self.target]
         if self.duration:
-            self.durations = self.DURATIONS[:self.DURATIONS.index(self.duration) + 1]
+            self.durations = _relevant_durations(self.duration)
 
     @staticmethod
-    def _get_duration(dict_, duration):
+    def _get_time(dict_, duration):
         dt = dict_['time']
-        return str(getattr(dt, duration))
+        return getattr(dt, duration)
 
-    def _find_duration(self, d):
+    def _find_time(self, d):
         if not self.duration:
             return "all time"
-        return ".".join(self._get_duration(d, duration) for duration in self.durations)
+        return _construct_relevant_date(d['time'], self.duration)
 
     def _find_owner(self, d):
         return self.owner + ": " + str(d[self.owner])
 
     def key(self, d):
-        return (self.target, self._find_duration(d), self._find_owner(d))
+        return Key(self.target, self._find_owner(d), self.duration, self._find_time(d))
 
     def __str__(self):
         return "Field(%s, %s, %s)" % (self.target, self.duration, self.owner)
